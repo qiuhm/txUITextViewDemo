f1d6e518f5732d69130e6b96eeb8cbfeca6921b4
萱的软件测试博客 <qiuhmm@gmail.com>
2013-12-03 14:28:08 +0800

    Initial Commit

---
+++ txUITextViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  txUITextViewDemo
+//
+//  Created by tingxuan.qhm on 13-12-3.
+//  Copyright (c) 2013年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "txAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([txAppDelegate class]));
+    }
+}
---
+++ txUITextViewDemo/txAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  txAppDelegate.h
+//  txUITextViewDemo
+//
+//  Created by tingxuan.qhm on 13-12-3.
+//  Copyright (c) 2013年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface txAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
